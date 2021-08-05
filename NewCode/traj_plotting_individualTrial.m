ee9c76d3082aa51fc969b6453b34eba61a6596ba
Akira Nagamori <nagamori@usc.edu>
2021-08-05 10:10:14 -0700

    data

--- NewCode/traj_plotting_individualTrial.m
+++ NewCode/traj_plotting_individualTrial.m
@@ -2,7 +2,7 @@ close all
 clear all
 clc
 
-data_folder = 'C:\Users\anaga\Documents\GitHub\Joystick-Analysis\Data\Box_3_F_102320_CT\072921_30_100_030_000_360_000_360_00';
+data_folder = 'C:\Users\anaga\Documents\GitHub\Joystick-Analysis\Data\Box_3_F_102320_CT\080221_60_100_030_000_360_000_360_00';
 % 072321_60_100_050_000_360_000_360_00
 % 072521_60_100_050_000_360_000_360_00
 % 072621_60_100_050_000_360_000_360_00
@@ -10,11 +10,13 @@ cd(data_folder)
 load('jstruct')
 cd('C:\Users\anaga\Documents\GitHub\Joystick-Analysis\NewCode')
 
+plotOpt = 1;
+
 nTrial = length(jstruct);
 max_radial_position = []; %zeros(1,length(js_reward));
 
 Fs = 1000;
-[b,a] = butter(4,20/(Fs*2),'low');
+[b,a] = butter(4,50/(Fs*2),'low');
 index_reward = [];
         
 for i = 1:nTrial
@@ -24,11 +26,11 @@ for i = 1:nTrial
 end
 
 hold_threshold = 30/100*6.35;
-outer_threshold = 30/100*6.35;
-max_distance = 110/100*6.35;
+outer_threshold = 60/100*6.35;
+max_distance = 80/100*6.35;
 theta = 0:0.01:2*pi;
 %%
-for j = 1 %:length(index_reward) %1:50 %3:32
+for j = 2 %1:length(index_reward) %1:50 %3:32
     
     n = index_reward(j);
     traj_x = filtfilt(b,a,jstruct(n).traj_x/100*6.35);
@@ -39,7 +41,7 @@ for j = 1 %:length(index_reward) %1:50 %3:32
     onset_js = jstruct(n).js_pairs_r(js_reward,1);
     offset_js = jstruct(n).js_pairs_r(js_reward,2);
    
-    for k = 94 %1:length(js_reward)
+    for k = 1:length(js_reward)
         a_diff = find(jstruct(n).np_pairs(:,1)-onset_js(k)<0);
     
         onset_np = jstruct(n).np_pairs(a_diff(end),1);
@@ -61,120 +63,134 @@ for j = 1 %:length(index_reward) %1:50 %3:32
         
         radial_position = sqrt(traj_x.^2+traj_y.^2);
         
-        if offset_js(k) == offset_trial
-            figure(k)
-            subplot(4,1,1)
-            plot(time,traj_x(start_time:end_time),'LineWidth',1)
-            xlim([time(1),time(end)])
-            ylabel('x-position (mm)')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
+        if plotOpt == 1
+            if offset_js(k) == offset_trial
+                figure(k)
+                subplot(4,1,1)
+                plot(time,traj_x(start_time:end_time),'LineWidth',1)
+                xlim([time(1)-0.1,time(end)])
+                ylabel('x-position (mm)')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+                hold on 
+                subplot(4,1,2)
+                plot(time,traj_y(start_time:end_time),'LineWidth',1)
+                xlim([time(1)-0.1,time(end)])
+                xlabel('Time (s)')
+                ylabel('y-position (mm)')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+                hold on
+                subplot(4,1,3)
+                plot(time,radial_position(start_time:end_time),'LineWidth',1)
+                hold on 
+                plot([time(1) time(end)],[hold_threshold hold_threshold],'--','color','k','LineWidth',1)
+                plot([time(1) time(end)],[outer_threshold outer_threshold],'color','g','LineWidth',1)
+                plot([time(1) time(end)],[max_distance max_distance],'color','g','LineWidth',1)
+                xlim([time(1)-0.1,time(end)])
+                ylabel('radial distance (mm)')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+                subplot(4,1,4)
+                plot(time(onset_reward(k)-onset_js(k)),0.1,'.r', 'MarkerSize',10)
+                %plot([time(onset_reward(k)-onset_js(k)) time(onset_reward(k)-onset_js(k))],[0 1],'LineWidth',1)
+                hold on 
+                plot(0,0.3,'.b', 'MarkerSize',10)
+                plot(time(offset_trial-onset_js(k)),0.5,'.m', 'MarkerSize',10)
+                plot(time(offset_js(k)-onset_js(k)),0.7,'.k', 'MarkerSize',10)
+                xlim([time(1)-0.1,time(end)])
+                legend('Reward','Trial Onset','Trial Offset','Joystick Offest')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+            elseif offset_np == offset_trial
+                figure(k)
+                subplot(4,1,1)
+                plot(time,traj_x(start_time:end_time),'LineWidth',1)
+                xlim([time(1)-0.1,time(end)])
+                ylabel('x-position (mm)')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+                hold on 
+                subplot(4,1,2)
+                plot(time,traj_y(start_time:end_time),'LineWidth',1)
+                xlim([time(1)-0.1,time(end)])
+                xlabel('Time (s)')
+                ylabel('y-position (mm)')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+                hold on
+                subplot(4,1,3)
+                plot(time,radial_position(start_time:end_time),'LineWidth',1)
+                hold on 
+                plot([time(1) time(end)],[hold_threshold hold_threshold],'--','color','k','LineWidth',1)
+                plot([time(1) time(end)],[outer_threshold outer_threshold],'color','g','LineWidth',1)
+                plot([time(1) time(end)],[max_distance max_distance],'color','g','LineWidth',1)
+                xlim([time(1)-0.1,time(end)])
+                ylabel('radial distance (mm)')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+                subplot(4,1,4)
+                plot(time(onset_reward(k)-onset_js(k)),0.1,'.r', 'MarkerSize',10)
+                %plot([time(onset_reward(k)-onset_js(k)) time(onset_reward(k)-onset_js(k))],[0 1],'LineWidth',1)
+                hold on 
+                plot(0,0.3,'.b', 'MarkerSize',10)
+                plot(time(offset_trial-onset_js(k)),0.5,'.m', 'MarkerSize',10)
+                plot(time(offset_np-onset_js(k)),0.7,'.k', 'MarkerSize',10)
+                xlim([time(1)-0.1,time(end)])
+                legend('Reward','Trial Onset','Trial Offset','Nose Poke Offest')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+            elseif offset_js_l == offset_trial
+                figure(k)
+                subplot(4,1,1)
+                plot(time,traj_x(start_time:end_time),'LineWidth',1)
+                xlim([time(1)-0.1,time(end)])
+                ylabel('x-position (mm)')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+                hold on 
+                subplot(4,1,2)
+                plot(time,traj_y(start_time:end_time),'LineWidth',1)
+                xlim([time(1)-0.1,time(end)])
+                xlabel('Time (s)')
+                ylabel('y-position (mm)')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+                hold on
+                subplot(4,1,3)
+                plot(time,radial_position(start_time:end_time),'LineWidth',1)
+                hold on 
+                plot([time(1) time(end)],[hold_threshold hold_threshold],'--','color','k','LineWidth',1)
+                plot([time(1) time(end)],[outer_threshold outer_threshold],'color','g','LineWidth',1)
+                plot([time(1) time(end)],[max_distance max_distance],'color','g','LineWidth',1)
+                xlim([time(1)-0.1,time(end)])
+                ylabel('radial distance (mm)')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+                subplot(4,1,4)
+                plot(time(onset_reward(k)-onset_js(k)),0.1,'.r', 'MarkerSize',10)
+                %plot([time(onset_reward(k)-onset_js(k)) time(onset_reward(k)-onset_js(k))],[0 1],'LineWidth',1)
+                hold on 
+                plot(0,0.3,'.b', 'MarkerSize',10)
+                plot(time(offset_trial-onset_js(k)),0.5,'.m', 'MarkerSize',10)
+                plot(time(offset_js_l-onset_js(k)),0.7,'.k', 'MarkerSize',10)
+                xlim([time(1)-0.1,time(end)])
+                legend('Reward','Trial Onset','Trial Offset','Touch 2 Offest')
+                set(gca,'TickDir','out');
+                set(gca,'box','off')
+            end
+        elseif plotOpt == 2
+            figure(1)
+            plot(traj_x(start_time:end_time),traj_y(start_time:end_time),'LineWidth',1)
             hold on 
-            subplot(4,1,2)
-            plot(time,traj_y(start_time:end_time),'LineWidth',1)
-            xlim([time(1),time(end)])
-            xlabel('Time (s)')
-            ylabel('y-position (mm)')
-            set(gca,'TickDir','out');
+            xlim([-7 7])
+            ylim([-7 7])
+            set(gca,'TickDir','out')
             set(gca,'box','off')
             hold on
-            subplot(4,1,3)
-            plot(time,radial_position(start_time:end_time),'LineWidth',1)
-            hold on 
-            plot([time(1) time(end)],[hold_threshold hold_threshold],'--','color','k','LineWidth',1)
-            plot([time(1) time(end)],[outer_threshold outer_threshold],'color','g','LineWidth',1)
-            plot([time(1) time(end)],[max_distance max_distance],'color','g','LineWidth',1)
-            xlim([time(1),time(end)])
-            ylabel('radial distance (mm)')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
-            subplot(4,1,4)
-            plot(time(onset_reward(k)-onset_js(k)),0.1,'.r', 'MarkerSize',10)
-            %plot([time(onset_reward(k)-onset_js(k)) time(onset_reward(k)-onset_js(k))],[0 1],'LineWidth',1)
-            hold on 
-            plot(0,0.3,'.b', 'MarkerSize',10)
-            plot(time(offset_trial-onset_js(k)),0.5,'.m', 'MarkerSize',10)
-            plot(time(offset_js(k)-onset_js(k)),0.7,'.k', 'MarkerSize',10)
-            xlim([time(1)-0.1,time(end)])
-            legend('Reward','Trial Onset','Trial Offset','Joystick Offest')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
-        elseif offset_np == offset_trial
-            figure(k)
-            subplot(4,1,1)
-            plot(time,traj_x(start_time:end_time),'LineWidth',1)
-            xlim([time(1),time(end)])
-            ylabel('x-position (mm)')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
-            hold on 
-            subplot(4,1,2)
-            plot(time,traj_y(start_time:end_time),'LineWidth',1)
-            xlim([time(1),time(end)])
-            xlabel('Time (s)')
-            ylabel('y-position (mm)')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
-            hold on
-            subplot(4,1,3)
-            plot(time,radial_position(start_time:end_time),'LineWidth',1)
-            hold on 
-            plot([time(1) time(end)],[hold_threshold hold_threshold],'--','color','k','LineWidth',1)
-            plot([time(1) time(end)],[outer_threshold outer_threshold],'color','g','LineWidth',1)
-            plot([time(1) time(end)],[max_distance max_distance],'color','g','LineWidth',1)
-            xlim([time(1),time(end)])
-            ylabel('radial distance (mm)')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
-            subplot(4,1,4)
-            plot(time(onset_reward(k)-onset_js(k)),0.1,'.r', 'MarkerSize',10)
-            %plot([time(onset_reward(k)-onset_js(k)) time(onset_reward(k)-onset_js(k))],[0 1],'LineWidth',1)
-            hold on 
-            plot(0,0.3,'.b', 'MarkerSize',10)
-            plot(time(offset_trial-onset_js(k)),0.5,'.m', 'MarkerSize',10)
-            plot(time(offset_np-onset_js(k)),0.7,'.k', 'MarkerSize',10)
-            xlim([time(1)-0.1,time(end)])
-            legend('Reward','Trial Onset','Trial Offset','Nose Poke Offest')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
-        elseif offset_js_l == offset_trial
-            figure(k)
-            subplot(4,1,1)
-            plot(time,traj_x(start_time:end_time),'LineWidth',1)
-            xlim([time(1),time(end)])
-            ylabel('x-position (mm)')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
-            hold on 
-            subplot(4,1,2)
-            plot(time,traj_y(start_time:end_time),'LineWidth',1)
-            xlim([time(1),time(end)])
-            xlabel('Time (s)')
-            ylabel('y-position (mm)')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
-            hold on
-            subplot(4,1,3)
-            plot(time,radial_position(start_time:end_time),'LineWidth',1)
-            hold on 
-            plot([time(1) time(end)],[hold_threshold hold_threshold],'--','color','k','LineWidth',1)
-            plot([time(1) time(end)],[outer_threshold outer_threshold],'color','g','LineWidth',1)
-            plot([time(1) time(end)],[max_distance max_distance],'color','g','LineWidth',1)
-            xlim([time(1),time(end)])
-            ylabel('radial distance (mm)')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
-            subplot(4,1,4)
-            plot(time(onset_reward(k)-onset_js(k)),0.1,'.r', 'MarkerSize',10)
-            %plot([time(onset_reward(k)-onset_js(k)) time(onset_reward(k)-onset_js(k))],[0 1],'LineWidth',1)
-            hold on 
-            plot(0,0.3,'.b', 'MarkerSize',10)
-            plot(time(offset_trial-onset_js(k)),0.5,'.m', 'MarkerSize',10)
-            plot(time(offset_js_l-onset_js(k)),0.7,'.k', 'MarkerSize',10)
-            xlim([time(1)-0.1,time(end)])
-            legend('Reward','Trial Onset','Trial Offset','Touch 2 Offest')
-            set(gca,'TickDir','out');
-            set(gca,'box','off')
+            plot(hold_threshold*cos(theta),hold_threshold*sin(theta),'--','color','k')
+            plot(outer_threshold*cos(theta),outer_threshold*sin(theta),'color','g')
+            plot(max_distance*cos(theta),max_distance*sin(theta),'color','g')
         end
 %   
 %         
