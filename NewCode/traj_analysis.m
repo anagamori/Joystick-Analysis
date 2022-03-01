56aec43ea9b8d4ae261c2c2135175655c662070a
anagamori <nagamori@usc.edu>
2022-03-01 09:59:22 -0800

    Update traj_analysis.m

--- NewCode/traj_analysis.m
+++ NewCode/traj_analysis.m
@@ -33,17 +33,55 @@ theta = 0:0.01:2*pi;
 
 idx = 1;
 
-for i = 1:10 %:nTrial
+for i = 11:20 %:nTrial
+    radial_pos_2 = js_reach(i).radial_pos_2;
+    mag_vel_2 = js_reach(i).mag_vel_2;
+    RoC_2 = js_reach(i).RoC_2;
+    [min_vel_2,loc_min_vel_2] = findpeaks(-mag_vel_2);
+    [min_RoC_2,loc_min_RoC_2] = findpeaks(-RoC_2);
+    A_2 = repmat(loc_min_RoC_2',[1 length(loc_min_vel_2)]);
+    [minValue_2,closestIndex_2] = min(abs(A_2-loc_min_vel_2),[],1);
+    loc_min_vel_2(minValue_2>2) = [];
+    closestIndex_2(minValue_2>2) = [];
+    
+    % velocity minimum closest to reward 
+    [~,idx_min_vel_2_reward] = min(abs(loc_min_vel_2-1*Fs_js));
+    loc_min_vel_2_reward = loc_min_vel_2(idx_min_vel_2_reward);
     
     time_reach = [1:length(js_reach(i).radial_pos_2)]./Fs_js;
     figure(idx)
+    ax1 = subplot(3,1,1);
+    plot(time_reach,radial_pos_2,'k','LineWidth',1)
+    hold on
+    plot(time_reach(loc_min_vel_2_reward),radial_pos_2(loc_min_vel_2_reward),'o','color','b','LineWidth',2)
+    yline(hold_threshold,'--','color','k','LineWidth',2)
+    yline(outer_threshold,'color','g','LineWidth',2)
+    yline(max_distance,'color','g','LineWidth',2)
+    ax2 = subplot(3,1,2);
+    plot(time_reach,mag_vel_2,'k','LineWidth',1)
+    hold on 
+    plot(time_reach(loc_min_vel_2),mag_vel_2(loc_min_vel_2),'o','color','r')
+    plot(time_reach(loc_min_vel_2_reward),mag_vel_2(loc_min_vel_2_reward),'o','color','b','LineWidth',2)
+    ax3 = subplot(3,1,3);
+    plot(time_reach,js_reach(i).RoC_2,'k','LineWidth',1)
+    hold on 
+    plot(time_reach(loc_min_vel_2),RoC_2(loc_min_vel_2),'o','color','r')
+    plot(time_reach(loc_min_vel_2_reward),RoC_2(loc_min_vel_2_reward),'o','color','b','LineWidth',2)
+    set(gca, 'YScale', 'log')
+    linkaxes([ax1 ax2 ax3],'x')
+    
+    figure(idx+1)
     subplot(2,1,1)
-    plot(time_reach,js_reach(i).radial_pos_2,'k','LineWidth',1)
+    plot(time_reach,js_reach(i).radial_pos,'k','LineWidth',1)
+    hold on
+    yline(hold_threshold,'--','color','k','LineWidth',2)
+    yline(outer_threshold,'color','g','LineWidth',2)
+    yline(max_distance,'color','g','LineWidth',2)
     subplot(2,1,2)
-    plot(time_reach,js_reach(i).mag_vel_2,'k','LineWidth',1)
+    plot(time_reach,js_reach(i).mag_vel,'k','LineWidth',1)
     
         
-    figure(idx+1)
+    figure(idx+2)
     plot(js_reach(i).traj_x_2,js_reach(i).traj_y_2)
     hold on
     plot(js_reach(i).traj_x_2(1*Fs_js),js_reach(i).traj_y_2(1*Fs_js),'o','LineWidth',2)
